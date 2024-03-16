a9c6c5e0a3cec4d40b266235db4d348e49d02766
Rohanchikkalkar <rithikrohan007@gmail.com>
2024-03-16 13:08:59 +0530

    install webpack-dev-server, change mode to development and add source maps

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "TodoList",
@@ -11,12 +16,17 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
