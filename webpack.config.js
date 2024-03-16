b5a3ccf42b9b756f8892dfeebeecb494d39bbc07
Rohanchikkalkar <rithikrohan007@gmail.com>
2024-03-16 13:02:34 +0530

    install style-loader, css-loader & configure webpack to use it

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,21 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-
   plugins: [
     new HtmlWebpackPlugin({
       title: "TodoList",
     }),
   ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
